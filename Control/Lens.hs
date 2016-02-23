966620887c9c1c64af8a05ccee689bc9544636e4
David Fox <dsf@seereason.com>
2016-02-22 16:43:05 -0800

    Add microlens-mtl

--- Control/Lens.hs
+++ Control/Lens.hs
@@ -1,9 +1,11 @@
 module Control.Lens
     ( module Lens.Micro
     , module Lens.Micro.Extras
+    , module Lens.Micro.Mtl
     , module Lens.Micro.TH
     ) where
 
 import Lens.Micro
 import Lens.Micro.Extras
+import Lens.Micro.Mtl
 import Lens.Micro.TH
